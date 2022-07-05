594d7e3efa9206e579602c44ac97cd3e640a7d84
Adam Lagevik <adamlagevik@Adams-MacBook-Pro-2.local>
2022-07-05 15:51:18 +0200

    azure test

---
+++ client/build/precache-manifest.fabf12b889a1ffe4e67fa46b21c671dd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f87ea88fe3e354df732b",
+    "url": "/static/css/main.5076fd48.chunk.css"
+  },
+  {
+    "revision": "f87ea88fe3e354df732b",
+    "url": "/static/js/main.f87ea88f.chunk.js"
+  },
+  {
+    "revision": "f0ff088a22a065b3a207",
+    "url": "/static/js/1.f0ff088a.chunk.js"
+  },
+  {
+    "revision": "74710663e54db2f664cc",
+    "url": "/static/js/2.74710663.chunk.js"
+  },
+  {
+    "revision": "7af89477dc2ff582f291",
+    "url": "/static/js/runtime~main.7af89477.js"
+  },
+  {
+    "revision": "8283f021129ec15b1abee12571ad045a",
+    "url": "/static/media/logo.8283f021.png"
+  },
+  {
+    "revision": "37108951df2adaf9f3cbb5f7e3b6ccc7",
+    "url": "/static/media/tmp0.37108951.png"
+  },
+  {
+    "revision": "ac66d8160888a82707bc5a432f8ffa91",
+    "url": "/static/media/graph.ac66d816.png"
+  },
+  {
+    "revision": "8b209623f7384f3a34c4e76efeed08d7",
+    "url": "/static/media/IRR.8b209623.png"
+  },
+  {
+    "revision": "fc48c8864f8288ed15abb1f11fb30fa6",
+    "url": "/index.html"
+  }
+];
